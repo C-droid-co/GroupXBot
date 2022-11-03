e587a839da8bbbf91951aab6a59f2f13021f7b88
C-droid-co <103402546+C-droid-co@users.noreply.github.com>
2022-11-03 11:02:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -15,16 +15,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get it from my.telegram.org
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("27339814"))
+API_HASH = getenv("d9edbf2ab27db57c22448d28125d345c")
 
 ## Get it from @Botfather in Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("5651816564:AAG1CWSnWFcjHy9lRFhMfm5uY8130RrvM-4")
 
 # Database to save your chats and stats... Get MongoDB:-  https://notreallyshikhar.gitbook.io/yukkimusicbot/deployment/mongodb#4.-youll-see-a-deploy-cloud-database-option.-please-select-shared-hosting-under-free-plan-here
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_URI = getenv("MONGO_DB_URI", mongodb+srv://Admin:Admin@cluster0.cjhcye4.mongodb.net/?retryWrites=true&w=majority)
 
 # SUDO USERS
 SUDO_USER = list(
-    map(int, getenv("SUDO_USER", "").split())
+    map(int, getenv("SUDO_USER", "5465891857").split())
 )  # Input type must be interger
